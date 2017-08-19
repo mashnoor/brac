fd437329a6febb41c5624e1ef8be0d1a14946fef
root <root@radioruet.com>
2017-08-20 01:53:39 +0600

    From Server

--- webhook.py
+++ webhook.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from brac import getPrediction
-
+from dateutil import parser
 import datetime
 
 
@@ -15,10 +15,13 @@ def gettime():
     weekday = request.form.get('weekday')
     vehicle = request.form.get('vehicle')
     estimated_second = float(getPrediction(start, stop, time_H, time_M, weekday, vehicle))
-    now = datetime.datetime.now()
+    #now = datetime.datetime.now()
+    now = parser.parse(str(time_H) + ":" + str(time_M))
     estimated_time = now + datetime.timedelta(seconds=estimated_second)
     estimated_hour = estimated_second/3600.0
-    msg = "Time Required (Apprx.) : " + str(estimated_hour) + " hours\n"
+    estimated_minute = estimated_hour - int(estimated_hour)
+    estimated_minute = int(estimated_minute*60)
+    msg = "Time Required (Apprx.) : " + str(int(estimated_hour)) + " hours and " + str(estimated_minute) + " minutes\n"
     
     msg += "You will reach there at " + str(estimated_time.hour) + ":" + str(estimated_time.minute)
     return msg
@@ -30,5 +33,25 @@ def getPlaces():
     f.close()
     return data
 
+@app.route('/feedback', methods=["POST"])
+def feedback():
+    fb = request.form.get('feedback')
+    with open("feedbacks.txt", "a") as f:
+        f.write(str(fb) + "\n")
+    return "Thanks for your feedback"
+
+@app.route('/contribute', methods=["POST"])
+def contribute():
+    start_place = request.form.get('start_place')
+    stop_place = request.form.get('stop_place')
+    time_start = request.form.get('time_start')
+    duration = request.form.get('duration')
+    vehicle = request.form.get('vehicle')
+    weekday = request.form.get('weekday')
+    string = str(start_place) + " " + str(stop_place) + " " + str(time_start) + " " + str(duration) + " " + str(vehicle) + " " + str(weekday)+"\n"
+    with open("contribution.txt", "a") as f:
+        f.write(string)
+    return "Thanks for  you contribution!"
+
 if __name__=="__main__":
-    app.run(port=1234)
+    app.run(port=1234, host='0.0.0.0')
